347e883f3a1a303a9a9aa39555a2e741f77fa079
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-03-22 11:47:33 +0100

    fixed REDFT00 and REDFT10 examples

--- img/plot_1d_redft_examples.py
+++ img/plot_1d_redft_examples.py
@@ -11,8 +11,6 @@ from numpy.random import default_rng
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
-#%% REDFT00 
-
 # compute the REDFT00 as defined in the FFTW reference manual
 def redft00(arr):
     n = len(arr)
@@ -24,14 +22,53 @@ def redft00(arr):
             out[k] += 2.0 * arr[j] * np.cos(np.pi*j*k/(n-1))
     return out
 
+# compute the REDFT10 as defined in the FFTW reference manual
+def redft10(arr):
+    n = len(arr)
+    out = np.zeros([n])
+    for k in range(n):
+        for j in range(n):
+            out[k] += 2.0 * arr[j] * np.cos(np.pi*(j+0.5)*k/n)
+    return out
+
+# compute the REDFT01 as defined in the FFTW reference manual
+def redft01(arr):
+    n = len(arr)
+    out = np.zeros([n])
+    for k in range(n):
+        out[k] += arr[0]
+        for j in range(1, n):
+            out[k] += 2.0 * arr[j] * np.cos(np.pi*j*(k+0.5)/n)
+    return out
+
 # evaluate the REDFT00 at all points given in x
 def eval_redft00(arr, x):
+    n = len(arr)
     y = np.zeros_like(x)
     y += arr[0]
     for j in range(1,n-1):
         y += 2.0*arr[j]*np.cos(np.pi*j*x)
     y += arr[-1]*np.cos(np.pi*(n-1)*x)
     return y
+
+# evaluate the REDFT10 at all points given in x
+def eval_redft10(arr, x):
+    n = len(arr)
+    y = np.zeros_like(x)
+    for j in range(n):
+        y += 2.0*arr[j]*np.cos(np.pi*(j+0.5)*x)
+    return y
+
+# evaluate the REDFT01 at all points given in x
+def eval_redft01(arr, x):
+    n = len(arr)
+    y = np.zeros_like(x)
+    y += arr[0]
+    for j in range(1, n):
+        y += 2.0*arr[j]*np.cos(np.pi*j*(x+0.5/n))
+    return y
+
+#%% REDFT00 
 
 # input size for REDFT00
 n = 5
@@ -41,22 +78,22 @@ N = 2*(n-1)
 
 # random Fourier coefficients
 rng = default_rng(seed=42)
-r2r_in = rng.uniform(size=n)
+r2r_out = rng.uniform(size=n)
 
-# compute output of REDFT00
-r2r_out = redft00(r2r_in)/n
+# compute input of REDFT00 by REDFT00 (the inverse of REDFT00)
+r2r_in = redft00(r2r_out)/n
 
 # extend to equivalent DFT size
-full_out_x = np.arange(n, 2*n-2)
-full_out_y = np.zeros([n-2])
-for i in range(n-2):
+full_in_x = np.arange(n, N)
+full_in_y = np.zeros([N-n])
+for i in range(N-n):
     # even symmetry around n-1
-    full_out_y[i] = r2r_out[-i-2]
+    full_in_y[i] = r2r_in[-i-2]
 
 # sample at finer intervals
 nFine = 1024
 x = np.linspace(-n+1, N, nFine)
-y = eval_redft00(r2r_in, x/(n-1))/n
+y = eval_redft00(r2r_out, x/(n-1))/n
 
 plt.figure(figsize=(5,3))
 
@@ -71,18 +108,18 @@ plt.axvline(n-1, ls='--', color='r')
 # highlight array contents
 plt.axvspan(0-0.25, (n-1)+0.25, alpha=0.3, color='gray')
 
-# plot actual REDFT00 output
-plt.plot(r2r_out, 'bo')
+# plot actual REDFT00 input
+plt.plot(r2r_in, 'bo')
 
 # label individual points
 for i in range(n):
-    plt.text(i+0.2, r2r_out[i]-0.03, chr(ord("a")+i))
+    plt.text(i+0.2, r2r_in[i]-0.03, chr(ord("a")+i))
 
 # plot data extending REDFT00 to full DFT
-plt.plot(full_out_x, full_out_y, 'bo')
+plt.plot(full_in_x, full_in_y, 'bo')
 
 for i in range(n-2):
-    plt.text(full_out_x[i]+0.2, full_out_y[i]-0.03, chr(ord("a")+(n-2-i)))
+    plt.text(full_in_x[i]+0.2, full_in_y[i]-0.03, chr(ord("a")+(n-2-i)))
 
 # only integer xaxis ticks at intervals of 1
 plt.gca().xaxis.set_major_locator(MaxNLocator(steps=(1,10), integer=True))
@@ -95,10 +132,34 @@ plt.tight_layout()
 plt.savefig("redft00.png")
 
 #%% REDFT10
-n = int(N/2)
+n = 4
+
+# logical size of equivalent DFT (for REDFT10)
+N = 2*n
+
+# random Fourier coefficients
+rng = default_rng(seed=40)
+r2r_out = rng.uniform(size=n)
+
+# compute input of REDFT10 by using REDFT01 (the inverse of REDFT10)
+r2r_in = redft01(r2r_out)/n
+
+# extend to equivalent DFT size
+full_in_x = np.arange(n, N)
+full_in_y = np.zeros([N-n])
+for i in range(N-n):
+    # even symmetry around n-0.5
+    full_in_y[i] = r2r_in[-i-1]
+
+# sample "inputs" at finer intervals
+nFine = 1024
+x = np.linspace(-n, N, nFine)
+y = eval_redft01(r2r_out, x/n)/n
 
 plt.figure(figsize=(5,3))
-plt.plot(t_ref[int(n_ref/N/2):-int(n_ref/N/2)]*N-0.5, y_ref[int(n_ref/N/2):-int(n_ref/N/2)], '-', color='gray', linewidth=0.5)
+
+plt.plot(x, y, '-', color='gray', linewidth=0.5)
+
 plt.axhline(0, ls='-', color='k')
 plt.axvline(0, ls='-', color='k')
 
@@ -109,17 +170,17 @@ plt.axvline(n-0.5, ls='--', color='r')
 # highlight array contents
 plt.axvspan(0-0.25, (n-1)+0.25, alpha=0.3, color='gray')
 
-plt.plot(t, y1, 'bo')
+plt.plot(r2r_in, 'bo')
 
-for i in range(N):
-    if i==n-1:
-        plt.text(t[int(i+N/2)]-0.7, y1[int(i+N/2)], chr(ord("a")+i))
-    elif i==N-1:
-        plt.text(t[int(i+N/2)]-0.7, y1[int(i+N/2)], chr(ord("a")+(N-i-1)))
-    elif i>=n:
-        plt.text(t[int(i+N/2)]+0.2, y1[int(i+N/2)], chr(ord("a")+(N-i-1)))
-    else:
-        plt.text(t[int(i+N/2)]+0.2, y1[int(i+N/2)], chr(ord("a")+i))
+# label individual points
+for i in range(n-1):
+    plt.text(i+0.2, r2r_in[i]-0.03, chr(ord("a")+i))
+plt.text(n-1-0.6, r2r_in[n-1]-0.03, chr(ord("a")+n-1))
+
+plt.plot(full_in_x, full_in_y, 'bo')
+
+for i in range(n):
+    plt.text(full_in_x[i]+0.2, full_in_y[i]-0.03, chr(ord("a")+(n-1-i)))
 
 # only integer xaxis ticks
 plt.gca().xaxis.set_major_locator(MaxNLocator(steps=(1,10), integer=True))
@@ -133,67 +194,67 @@ plt.savefig("redft10.png")
 
 #%% REDFT01
 
-plt.figure(figsize=(5,3))
-plt.plot(t_ref*N, y_ref_2, '-', color='gray', linewidth=0.5)
-plt.axhline(0, ls='-', color='k')
-plt.axvline(0, ls='-', color='k')
+# plt.figure(figsize=(5,3))
+# plt.plot(t_ref*N, y_ref_2, '-', color='gray', linewidth=0.5)
+# plt.axhline(0, ls='-', color='k')
+# plt.axvline(0, ls='-', color='k')
 
-# symmetry lines
-plt.axvline(0, ls='--', color='r')
-plt.axvline(n, ls='--', color='b')
+# # symmetry lines
+# plt.axvline(0, ls='--', color='r')
+# plt.axvline(n, ls='--', color='b')
 
-# highlight array contents
-plt.axvspan(0-0.25, (n-1)+0.25, alpha=0.3, color='gray')
+# # highlight array contents
+# plt.axvspan(0-0.25, (n-1)+0.25, alpha=0.3, color='gray')
 
-plt.plot(t, y2, 'bo')
+# plt.plot(t, y2, 'bo')
 
-for i in range(N):
-    if i>n:
-        plt.text(t[int(i+N/2)]+0.2, y2[int(i+N/2)], "-"+chr(ord("a")+(N-i)))
-    elif i<n:
-        plt.text(t[int(i+N/2)]+0.2, y2[int(i+N/2)], chr(ord("a")+i))
+# for i in range(N):
+#     if i>n:
+#         plt.text(t[int(i+N/2)]+0.2, y2[int(i+N/2)], "-"+chr(ord("a")+(N-i)))
+#     elif i<n:
+#         plt.text(t[int(i+N/2)]+0.2, y2[int(i+N/2)], chr(ord("a")+i))
 
-# only integer xaxis ticks
-plt.gca().xaxis.set_major_locator(MaxNLocator(steps=(1,10), integer=True))
-plt.xlim((-4.5, 8.5))
+# # only integer xaxis ticks
+# plt.gca().xaxis.set_major_locator(MaxNLocator(steps=(1,10), integer=True))
+# plt.xlim((-4.5, 8.5))
 
-plt.grid(True)
-plt.xlabel("j")
-plt.title("REDFT01 N=%d n=%d"%(N, n))
-plt.tight_layout()
-plt.savefig("redft01.png")
+# plt.grid(True)
+# plt.xlabel("j")
+# plt.title("REDFT01 N=%d n=%d"%(N, n))
+# plt.tight_layout()
+# plt.savefig("redft01.png")
 
 
 #%% REDFT11
 
-plt.figure(figsize=(5,3))
-plt.plot(t_ref[int(n_ref/N/2):-int(n_ref/N/2)]*N-0.5, y_ref_2[int(n_ref/N/2):-int(n_ref/N/2)], '-', color='gray', linewidth=0.5)
-plt.axhline(0, ls='-', color='k')
-plt.axvline(0, ls='-', color='k')
+# plt.figure(figsize=(5,3))
+# plt.plot(t_ref[int(n_ref/N/2):-int(n_ref/N/2)]*N-0.5, y_ref_2[int(n_ref/N/2):-int(n_ref/N/2)], '-', color='gray', linewidth=0.5)
+# plt.axhline(0, ls='-', color='k')
+# plt.axvline(0, ls='-', color='k')
 
-# symmetry lines
-plt.axvline(-0.5, ls='--', color='r')
-plt.axvline(n-0.5, ls='--', color='b')
+# # symmetry lines
+# plt.axvline(-0.5, ls='--', color='r')
+# plt.axvline(n-0.5, ls='--', color='b')
 
-# highlight array contents
-plt.axvspan(0-0.25, (n-1)+0.25, alpha=0.3, color='gray')
+# # highlight array contents
+# plt.axvspan(0-0.25, (n-1)+0.25, alpha=0.3, color='gray')
 
-plt.plot(t, y3, 'bo')
+# plt.plot(t, y3, 'bo')
 
-for i in range(N):
-    if i==n-1:
-        plt.text(t[int(i+N/2)]-0.6, y3[int(i+N/2)], chr(ord("a")+i))
-    elif i>=n:
-        plt.text(t[int(i+N/2)]+0.2, y3[int(i+N/2)], "-"+chr(ord("a")+(N-i-1)))
-    else:
-        plt.text(t[int(i+N/2)]+0.2, y3[int(i+N/2)], chr(ord("a")+i))
+# for i in range(N):
+#     if i==n-1:
+#         plt.text(t[int(i+N/2)]-0.6, y3[int(i+N/2)], chr(ord("a")+i))
+#     elif i>=n:
+#         plt.text(t[int(i+N/2)]+0.2, y3[int(i+N/2)], "-"+chr(ord("a")+(N-i-1)))
+#     else:
+#         plt.text(t[int(i+N/2)]+0.2, y3[int(i+N/2)], chr(ord("a")+i))
 
-# only integer xaxis ticks
-plt.gca().xaxis.set_major_locator(MaxNLocator(steps=(1,10), integer=True))
-plt.xlim((-4.5, 8.5))
+# # only integer xaxis ticks
+# plt.gca().xaxis.set_major_locator(MaxNLocator(steps=(1,10), integer=True))
+# plt.xlim((-4.5, 8.5))
 
-plt.grid(True)
-plt.xlabel("j")
-plt.title("REDFT11 N=%d n=%d"%(N, n))
-plt.tight_layout()
-plt.savefig("redft11.png")
+# plt.grid(True)
+# plt.xlabel("j")
+# plt.title("REDFT11 N=%d n=%d"%(N, n))
+# plt.tight_layout()
+# plt.savefig("redft11.png")
